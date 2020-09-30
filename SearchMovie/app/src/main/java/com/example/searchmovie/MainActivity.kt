df76c812fa15abf8b14d6df4ab28b1cda5350536
sangminkim <apfhdznzl@gmail.com>
2020-10-01 00:52:00 +0900

    Modified MainActivity for Retrofit

--- SearchMovie/app/src/main/java/com/example/searchmovie/MainActivity.kt
+++ SearchMovie/app/src/main/java/com/example/searchmovie/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.searchmovie
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.searchmovie.data.Movie
 import com.example.searchmovie.service.RetrofitClient
@@ -21,12 +22,24 @@ class MainActivity : AppCompatActivity() {
             RetrofitClient.getService().getMovieList(query).enqueue(object :
                 Callback<Movie> {
                 override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
-                    Log.d("sangmee", response.body().toString())
-                }
 
+                    if (response.isSuccessful) {
+                        val body = response.body()
+                        body?.let {
+                            // TODO: 2020/10/01 recylerview에 body.items 넣기
+                        }
+                    }
+                    else{
+                        showMessage(getString(R.string.net_error))
+                    }
+                }
                 override fun onFailure(call: Call<Movie>, t: Throwable) {
+                    showMessage(t.message.toString())
                 }
             })
         }
+    }
+    fun showMessage(msg: String){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
