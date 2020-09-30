7e4d72eadb6004121f46cf580c7ffbc9d768acf5
sangminkim <apfhdznzl@gmail.com>
2020-10-01 00:36:20 +0900

    ADD data class Items & Movie

---
+++ SearchMovie/app/src/main/java/com/example/searchmovie/data/Items.kt
@@ -0,0 +1,15 @@
+package com.example.searchmovie.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Items(
+
+    @SerializedName("title") val title: String,
+    @SerializedName("link") val link: String,
+    @SerializedName("image") val image: String,
+    @SerializedName("subtitle") val subtitle: String,
+    @SerializedName("pubDate") val pubDate: Int,
+    @SerializedName("director") val director: String,
+    @SerializedName("actor") val actor: String,
+    @SerializedName("userRating") val userRating: Double
+)
---
+++ SearchMovie/app/src/main/java/com/example/searchmovie/data/Movie.kt
@@ -0,0 +1,12 @@
+package com.example.searchmovie.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+
+    @SerializedName("lastBuildDate") val lastBuildDate: String,
+    @SerializedName("total") val total: Int,
+    @SerializedName("start") val start: Int,
+    @SerializedName("display") val display: Int,
+    @SerializedName("items") val items: List<Items>
+)
