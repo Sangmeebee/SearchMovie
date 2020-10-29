99be5a1adce6d840e02b442609193cd1e2ca2464
sangminkim <apfhdznzl@gmail.com>
2020-10-29 20:10:29 +0900

    ViewModel, LiveData - update MainViewModel using AAC ViewModel, LiveData

--- SearchMovie/app/src/main/java/com/example/searchmovie/observer/MainViewModel.kt
+++ SearchMovie/app/src/main/java/com/example/searchmovie/observer/MainViewModel.kt
@@ -1,44 +1,56 @@
 package com.example.searchmovie.observer
 
-import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.searchmovie.data.dataSource.local.LocalDataSourceImpl
 import com.example.searchmovie.data.dataSource.remote.RemoteDataSourceImpl
 import com.example.searchmovie.data.model.Items
 import com.example.searchmovie.data.repository.MovieSearchRepositoryImpl
 
-class MainViewModel {
+class MainViewModel : ViewModel() {
     private val repository =
         MovieSearchRepositoryImpl(LocalDataSourceImpl(), RemoteDataSourceImpl())
-    val query = ObservableField<String>()
-    val movieList = ObservableField<List<Items>>()
-    val showToast = ObservableField<Unit>()
-    val showDialog = ObservableField<Unit>()
+    val query = MutableLiveData<String>()
+    val movieList = MutableLiveData<List<Items>>()
+    val showToast = MutableLiveData<Boolean>(false)
+    val showDialog = MutableLiveData<Boolean>()
     var queryState = ""
+    val queryList = MutableLiveData<List<String>>()
 
     fun callMovieList() {
-        val query = query.get()
+        val query = query.value
         if (query.isNullOrBlank()) {
             queryState = "empty"
-            showToast.notifyChange()
-            movieList.set(arrayListOf())
+            showToast.value = true
+            movieList.value = arrayListOf()
         } else {
             repository.callMovieList(query, {
                 if (it.isEmpty()) {
                     queryState = "result_empty"
-                    showToast.notifyChange()
-                    movieList.set(arrayListOf())
+                    showToast.value = true
+                    movieList.value = arrayListOf()
                 } else {
                     queryState = "success"
-                    showToast.notifyChange()
-                    movieList.set(it)
+                    showToast.value = true
+                    movieList.value = it
                 }
             }, {
                 queryState = "error"
-                showToast.notifyChange()
-                showToast.notifyChange()
+                showToast.value = true
             })
         }
     }
 
-    fun callDialog() = showDialog.notifyChange()
+    fun callDialog() {
+        showDialog.value = true
+    }
+
+    fun callQueryList() {
+        queryList.value = repository.readRecentQuery()
+    }
+
+    fun dismissDialog() {
+        showDialog.value = false
+    }
+
 }
